faa0192cef682a66836da862b93032735de8d43e
jared huffstutler <jaredhuff85@gmail.com>
2019-04-13 01:42:32 -0500

    initial setup

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="clock-container">
+        <div className="my-5 bg-light rounded-circle border border-dark clock-body">
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
